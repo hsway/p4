edd924a64175175cc7dbabc69a795a9d71769959
Hank Sway <hank.sway@gmail.com>
2014-12-16 14:16:17 -0500

    testing responsive form design

--- app/views/user_login.blade.php
+++ app/views/user_login.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 
-<h1>Login</h1>
+<h2>Log In</h2>
+
+<br>
 
 @foreach($errors->all() as $message)
     <div class='error'>{{ $message }}</div>
@@ -10,22 +12,29 @@
 
 {{ Form::open(array('url' => 'login')) }}
 
+    <div class="form-group">
     {{-- Email address field. -------------------}}
     {{ Form::label('email', 'Email address') }}
-    {{ Form::email('email') }}<br />
+    {{ Form::email('email', '', array('class'=>'form-control', 'placeholder'=>'you@somewhere.com')) }}
+    </div>
 
+    <div class="form-group">
     {{-- Password field. ------------------------}}
     {{ Form::label('password', 'Password') }}
-    {{ Form::password('password') }}<br />
+    {{ Form::password('password', array('class'=>'form-control', 'placeholder'=>'******')) }}
+    </div>
 
+    <div class="checkbox">
     {{-- Remember me? ---------------------------}}
-    {{ Form::label('remember', 'Remember me?') }}
-    {{ Form::checkbox('remember') }}<br />
+    <label for="remember"><input name="remember" type="checkbox" value="1" id="remember"> Remember me?</label>
+    </div>
 
     {{-- Form submit button. --------------------}}
-    {{ Form::submit('Log in', array('class' => 'btn btn-primary')) }}
+    {{ Form::submit('Log In', array('class' => 'btn btn-primary')) }}
 
 {{ Form::close() }}
+
+<br>
 
 <p><a href="/password/remind">Need to reset your password?</p>
 
