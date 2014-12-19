0112530919c9c5ad7e6116c7d9e4108bbf91b28a
Hank Sway <hank.sway@gmail.com>
2014-12-19 14:05:20 -0600

    styling for welcome (logged out) page

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -2,17 +2,29 @@
 
 @section('content')
 
-<div id="home-wrapper">
-<div id="home-jumbo" class="jumbotron">
 
-	<h1>Run. Record. Repeat.</h1>
+<!-- <div class="jumbotron" style="overflow:hidden;">
 
-	<p>Track your miles and your shoes in one place.</p>
-
+	<div style="float:left;max-width:70%;display:inline-block;">
+		<h1>Run. Record. Repeat.</h1>
+		<p>Track your miles and your shoes in one place.</p>
+	</div>
+	
 	<img src="{{ URL::asset('img/rs-small.png') }}" alt="Run Simple logo" id="logo">
 
+</div> -->
+
+<div class="jumbotron">
+  <div id="jumbo-text">
+    <h1>Run. Record. Repeat.</h1>
+	<p>Track your miles and your shoes in one place.</p>
+  </div>
+
+  <div id="jumbo-image">
+    <img src="{{ URL::asset('img/rs-small.png') }}" alt="Run Simple logo" id="logo">
+  </div>
 </div>
-</div>
+
 
 <!-- http://stackoverflow.com/questions/26979512/css-placing-two-side-by-side-divs-on-the-same-height -->
 
